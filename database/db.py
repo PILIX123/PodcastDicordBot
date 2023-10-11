c4e3baf4ad15e4f8bed0a060588d0ea5d26aff77
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2023-10-11 02:40:09 -0400

    will look into create_pool later

--- database/db.py
+++ database/db.py
@@ -2,6 +2,7 @@ import asqlite
 
 class Database():
     async def init(self) -> None:
+        #asqlite.create_pool("list.sqlite")
         async with asqlite.connect("list.sqlite") as conn:
             async with conn.cursor() as cursor:
                 if((await (await cursor.execute("SELECT name FROM sqlite_master WHERE name='list'")).fetchone())["name"] is not None):
