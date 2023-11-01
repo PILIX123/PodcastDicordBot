43887bc299ff886aeb9540202a34d54f96e931a1
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2023-11-01 13:14:09 -0400

    changing the logic and adding tests

---
+++ tests/test_customAudio.py
@@ -0,0 +1,34 @@
+from utils.customAudio import CustomAudio
+from discord import FFmpegPCMAudio
+from pytest import MonkeyPatch
+
+
+def test_CustomAudio(monkeypatch: MonkeyPatch) -> None:
+    def mockInit(*args, **kwargs):
+        return None
+    monkeypatch.setattr(FFmpegPCMAudio, "__init__", mockInit)
+
+    customAudio = CustomAudio("", 0, 0)
+    assert customAudio.currentTimestamp == 0
+    assert customAudio.playstateId == 0
+
+
+def test_CustomAudio_beforeOptionMS(monkeypatch: MonkeyPatch) -> None:
+    def mockInit(*args, **kwargs):
+        return None
+    monkeypatch.setattr(FFmpegPCMAudio, "__init__", mockInit)
+
+    customAudio = CustomAudio("", 0, 0, before_options="-ss 1245ms")
+    assert customAudio.currentTimestamp == 1245
+    assert customAudio.playstateId == 0
+
+
+def test_CustomAudio_read(monkeypatch: MonkeyPatch) -> None:
+    def mockRead(*args, **kwargs):
+        return None
+    monkeypatch.setattr(FFmpegPCMAudio, "read", mockRead)
+
+    customAudio = CustomAudio("", 0, 0)
+    customAudio.read()
+    assert customAudio.playstateId == 0
+    assert customAudio.currentTimestamp == 20
--- utils/customAudio.py
+++ utils/customAudio.py
@@ -5,15 +5,12 @@ from utils.converters import Converters
 
 
 class CustomAudio(FFmpegPCMAudio):
-    def __init__(self, source: str | BufferedIOBase, timestamp: int, playstateId: str, *, executable: str = 'ffmpeg', pipe: bool = False, stderr: IO[str] | None = None, before_options: str | None = None, options: str | None = None) -> None:
+    def __init__(self, source: str | BufferedIOBase, timestamp: int, playstateId: int, *, executable: str = 'ffmpeg', pipe: bool = False, stderr: IO[str] | None = None, before_options: str | None = None, options: str | None = None) -> None:
         self.currentTimestamp = timestamp
         self.playstateId = playstateId
         if before_options is not None:
             if (before_options.startswith("-ss") and before_options.endswith("ms")):
                 self.currentTimestamp = int(before_options[4:-2])
-            elif (before_options.startswith("-ss")):
-                self.currentTimestamp = Converters.hourStrToMs(
-                    before_options[4:])
         super().__init__(source, executable=executable, pipe=pipe,
                          stderr=stderr, before_options=before_options, options=options)
 
