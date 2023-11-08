756a88c54630c8370a22c79bb930fd35e98800e4
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2023-11-08 05:34:25 -0500

    fixing basic stuff

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord import Intents, Interaction, Client, app_commands
 from vault.vault import Vault
 from database.db import Database
 from commands import commands
+from enums.enums import CommandEnum
 
 vault = Vault()
 intents = Intents.none()
@@ -21,41 +22,44 @@ async def connect(interaction: Interaction):
 
 @tree.command(name="stop", description="Stops audio")
 async def stop(interaction: Interaction):
-    await commands.stop(interaction,db,sessionMaker(engine))
+    await commands.stop(interaction, db, sessionMaker(engine))
 
 
 @tree.command(name="disconnect", description="Disconnects from a voice channel")
 async def disconnect(interaction: Interaction):
-    await commands.disconnect(interaction,db,sessionMaker(engine))
+    await commands.disconnect(interaction, db, sessionMaker(engine))
 
 
 @tree.command(name="subscribe", description="Subscribes the user to the given RSS feed")
 @app_commands.describe(url="URL pointing to the rss feed")
 async def subscribe(interaction: Interaction, url: str):
-    await commands.subscribe(interaction,url,db,sessionMaker(engine))
+    await commands.subscribe(interaction, url, db, sessionMaker(engine))
 
 
 @tree.command(name="list", description="List all podcast you are subscribed to")
 async def listing(interaction: Interaction):
-    await commands.list(interaction,db,sessionMaker(engine))
+    await commands.list(interaction, db, sessionMaker(engine))
 
 
 @tree.command(name="unsubscribe", description="Unsubscribes the user from the RSS feed")
 @app_commands.describe(name="**RESPECT CAPITALIZATION** Name of the podcast")
 async def unsubscribe(interaction: Interaction, name: str):
-    await commands.unsubscribe(interaction,name,db,sessionMaker(engine))
+    await commands.unsubscribe(interaction, name, db, sessionMaker(engine))
+
 
 @tree.command(name="play", description="Plays the podcast with the given name.")
 @app_commands.describe(name="**RESPECT CAPITALIZATION** Name of the podcast",
                        episode_number="The number of the episode wanted",
                        timestamp="**`HH:MM:SS` format only** Timestamp to start the episode at")
 async def play(interaction: Interaction, name: str, episode_number: None | int = None, timestamp: None | str = None):
-    await commands.play(interaction,name,episode_number,timestamp,db,sessionMaker(engine))
+    await commands.play(interaction, name, episode_number, timestamp, db, sessionMaker(engine))
+
 
 @tree.command(name="help", description="Explains the use of the commands")
 @app_commands.describe(command="Name of the command you want help with")
 async def help(interaction: Interaction, command: CommandEnum):
-    await commands.help(interaction,command)
+    await commands.help(interaction, command)
+
 
 async def getEngine():
     engine = create_async_engine(
